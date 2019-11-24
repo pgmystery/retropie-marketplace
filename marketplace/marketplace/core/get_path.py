d2da12759cf0e870b265ba795642e639811d4698
Phil <>
2019-11-24 23:59:00 +0100

    Clean up code in 'get_path.py'

--- marketplace/marketplace/core/get_path.py
+++ marketplace/marketplace/core/get_path.py
@@ -3,11 +3,9 @@ import sys
 
 
 def get_path():
-	path = None
 	if getattr(sys, 'frozen', False):
 		# frozen
-		path = os.path.dirname(sys.executable) + "/"
+		return os.path.dirname(sys.executable) + "/"
 	else:
 		# unfrozen
-		path = os.path.dirname(os.path.realpath(__file__)) + "/"
-	return path
+		return os.path.dirname(os.path.realpath(__file__)) + "/"
