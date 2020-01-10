3cf9abfd717679ce5032fbc1fb95f006e537dd11
P.G. Mystery <p.g.mystery@gmx.net>
2020-01-10 18:50:22 +0100

    Add "Go Back"-Button to the main menu and make it more user friendly

--- marketplace/marketplace/dialog/checklist.sh
+++ marketplace/marketplace/dialog/checklist.sh
@@ -7,12 +7,26 @@ BACKTITLE="$1"
 TITLE="$2"
 MENU="$3"
 
-CHOICE=$(dialog --clear \
-                --backtitle "$BACKTITLE" \
-                --title "$TITLE" \
-                --checklist "$MENU" \
-                $HEIGHT $WIDTH $CHOICE_HEIGHT \
-                "${GAMES[@]}" \
-                2>&1 >/dev/tty)
+if [ -z "$4" ]; then
+  CHOICE=$(dialog --clear \
+                  --backtitle "$BACKTITLE" \
+                  --title "$TITLE" \
+                  --checklist "$MENU" \
+                  $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                  "${GAMES[@]}" \
+                  2>&1 >/dev/tty)
+else
+  CHOICE=$(dialog --clear \
+                  --extra-button \
+                  --extra-label "$4" \
+                  --backtitle "$BACKTITLE" \
+                  --title "$TITLE" \
+                  --checklist "$MENU" \
+                  $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                  "${GAMES[@]}" \
+                  2>&1 >/dev/tty)
+fi
+
+CHOICE_BUTTON=$?
 
 clear
--- marketplace/marketplace/dialog/list.sh
+++ marketplace/marketplace/dialog/list.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
-LIST_CHOICE=$(dialog --clear \
-	                	  --backtitle "$1" \
-		                  --title "$1" \
-		                  --menu "$2" \
-		                  22 76 22 \
-		                  "${LIST[@]}" \
-		                  2>&1 >/dev/tty)
+if [ -z $3 ]; then
+  LIST_CHOICE=$(dialog --clear \
+                        --backtitle "$1" \
+                        --title "$1" \
+                        --menu "$2" \
+                        22 76 22 \
+                        "${LIST[@]}" \
+                        2>&1 >/dev/tty)
+else
+  LIST_CHOICE=$(dialog --clear \
+                        --extra-button \
+                        --extra-label "$3" \
+                        --backtitle "$1" \
+                        --title "$1" \
+                        --menu "$2" \
+                        22 76 22 \
+                        "${LIST[@]}" \
+                        2>&1 >/dev/tty)
+fi
+
+LIST_CHOICE_BUTTON=$?
 
 clear
