64ab168eb25084e73dde1304cd707049ed878739
Phil <>
2020-01-14 01:50:17 +0100

    Remove dead code

--- marketplace/marketplace/core/dialog/progressbox.py
+++ marketplace/marketplace/core/dialog/progressbox.py
@@ -6,7 +6,6 @@ class ProgressBox(Dialog):
 	def __init__(self, title, msg):
 		super(ProgressBox, self).__init__()
 		self.dialog = super(ProgressBox, self)._create_dialog("progressbox", title, msg)
-		# self.dialog.wait()
 
 	def write(self, text):
 		self.dialog.stdin.write(text)
